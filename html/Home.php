243be283066fab2f341091354aac3fe3a6f7f57a
Gessi Paniagua <gessi.paniagua@gmail.com>
2020-11-15 16:52:29 -0300

    estructura home

--- html/Home.php
+++ html/Home.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
   <title>VetLali</title>
   <meta charset="utf-8">
@@ -8,64 +9,53 @@
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+  <link href="https://getbootstrap.com/docs/4.1/examples/sticky-footer-navbar/sticky-footer-navbar.css" rel="stylesheet">
   <style>
-  .fakeimg {
-    height: 200px;
-    background: #17a2b8;
-  }
+    .fakeimg {
+      height: 200px;
+      background: #17a2b8;
+    }
   </style>
 </head>
-<body>
 
-<nav class="navbar navbar-expand-sm bg-dark navbar-dark">
-  <a class="navbar-brand" href="#">VetLali</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-  <div class="collapse navbar-collapse" id="collapsibleNavbar">
-    <ul class="navbar-nav">
-        <div class="btn-group">
-            <button type="button" class="btn btn-primary">Turnos</button>
-            <button type="button" class="btn btn-primary">Agregar Mascota</button>
+<body>
+  <header>
+    <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+      <a class="navbar-brand" href="#">VetAppWeb</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="collapsibleNavbar">
+        <ul class="navbar-nav">
+          <div class="btn-group">
+            <a class="btn btn-primary" href="./listaTurnos.php">Turnos</a>
+            <a class="btn btn-primary" href="./agregarMascotas.php">Agregar Mascota</a>
             <button type="button" class="btn btn-primary">Historial</button>
             <div class="btn-group">
-                <button type="button" class="btn btn-primary dropdown-toggle" data-toggle="dropdown">
+              <button type="button" class="btn btn-primary dropdown-toggle" data-toggle="dropdown">
                 Perfil
-                </button>
-                <div class="dropdown-menu">
-                    <a class="dropdown-item" href="#">Mi cuenta: <?= $this->user['nombre_apellido'] ?></a>
-                    <a class="dropdown-item" href="./logout.php">Salir</a>
-                </div>
+              </button>
+              <div class="dropdown-menu">
+                <a class="dropdown-item" href="#">Mi cuenta: <?= $this->user['nombre_apellido'] ?></a>
+                <a class="dropdown-item" href="./logout.php">Salir</a>
+              </div>
             </div>
-        </div> 
-  
-    </ul>
-  </div>
-</nav>
+          </div>
 
+        </ul>
+      </div>
+    </nav>
+  </header>
+  <main role="main" class="container">
 
+    Test
 
-
-
-
-<br/>
-<br/>
-<br/>
-<br/>
-<br/>
-<br/>
-<br/>
-<br/>
-
-
-<div class="jumbotron text-center" style="margin-bottom:0">
-  <p>Footer</p>
-</div>
-
+  </main>
+  <footer class="footer">
+    <div class="container text-center">
+      <span class="text-muted">Copyright © 2020 VetAppWeb by Gessi.</span>
+    </div>
+  </footer>
 </body>
+
 </html>
-
-
-
-
-
