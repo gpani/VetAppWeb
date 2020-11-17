02dd12d0b92692a20e597aa326baf1675e84ca3a
gessi <gessi.paniagua@gmail.com>
2020-11-16 21:15:01 -0300

    agrega estilo en login

--- html/Login.php
+++ html/Login.php
@@ -1,18 +1,30 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-  <title>Login</title>
-  <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
-  <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
-  <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-  <!------ Include the above in your HEAD tag ---------->
+    <title>Login</title>
+    <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+    <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <link href="https://getbootstrap.com/docs/4.1/examples/sticky-footer-navbar/sticky-footer-navbar.css" rel="stylesheet">
+
+    <style>
+
+    </style>
 </head>
 
 <body>
+    <header>
+        <nav class="navbar navbar-expand-sm" style="background-color: #77d6d3">
+            <a class="navbar-brand" href="#">
+                <img src="./assets/logo_size.jpg" alt="Logo" style="width:120px;">
+            </a>
+        </nav>
+    </header>
     <div id="login">
-        <?php if($this->error) { ?>
+        <?php if ($this->error) { ?>
             <h3><?= $this->error ?></h3>
-        <?php } ?> 
+        <?php } ?>
         <h3 class="text-center text-white pt-5">Login</h3>
         <div class="container">
             <div id="login-row" class="row justify-content-center align-items-center">
@@ -40,5 +52,12 @@
             </div>
         </div>
     </div>
+
+    <footer class="footer" style="background-color: #77d6d3">
+        <div class="container text-center">
+            <span class="text-muted">Copyright © 2020 VetAppWeb by Gessi.</span>
+        </div>
+    </footer>
 </body>
+
 </html>
